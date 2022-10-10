73579364af44ea8a2e28476efdc84b265dfa1763
Luan-vht3 <105639491+Luan-vht3@users.noreply.github.com>
2022-10-10 08:35:07 -0300

    Crie Docstrings

---
+++ src/modulo_inventario.py
@@ -0,0 +1,28 @@
+""" Função de listar os itens do inventário
+Imprime cada item do inventário.
+:rtype: None
+"""
+
+##################################################
+
+""" Função de adição de música ao inventário
+:param nome: nome da música a ser adicionada.
+:return: Mensagem de exceção ou None
+:rtype: str ou None.
+"""
+
+##################################################
+
+""" Função de remoção de música do inventário
+:param nome: nome da música a ser removida.
+:return: Mensagem de exceção ou None
+:rtype: str ou None.
+"""
+
+##################################################
+
+""" Função de busca de música no inventário
+:param nome: nome da música a ser buscada.
+:return: Mensagem de exceção ou booleano que informa se a música foi encontrada.
+:rtype: str ou bool.
+"""
